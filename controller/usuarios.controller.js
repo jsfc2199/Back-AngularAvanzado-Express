1fd22e173c6f73f21e8797a73daf3342a7cb4743
Juanse Franco <jsfc2199@gmail.com>
2024-07-19 13:24:32 -0500

    añadiendo validacion a que el correo sea único

--- controller/usuarios.controller.js
+++ controller/usuarios.controller.js
@@ -1,29 +1,48 @@
-const Usuario = require('../models/usuario')
+const { response } = require("express");
+
+const Usuario = require("../models/usuario");
 
 const getUsuarios = async (req, res) => {
-    const usuarios = await Usuario.find({}, 'nombre email role google') //podemos filtrar lo que queremos que retorne
+  const usuarios = await Usuario.find({}, "nombre email role google"); //podemos filtrar lo que queremos que retorne
   res.status(200).json({
     ok: true,
-    usuarios
+    usuarios,
   });
 };
 
-const crearUsuarios = async (req, res) => {
-    const {email, password, nombre} = req.body
+const crearUsuarios = async (req, res = response) => {
+  const { email, password, nombre } = req.body;
+
+  try {
+    //validar correo único
+    const existeEmail = await Usuario.findOne({ email });
+
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Correo ya existe",
+      });
+    }
 
     //creando sin validar
-    const usuario = new Usuario(req.body)
+    const usuario = new Usuario(req.body);
 
     //grabar en bd
-    await usuario.save()
+    await usuario.save();
 
     res.status(200).json({
       ok: true,
       usuario,
     });
-  };
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "error, revisar logs",
+    });
+  }
+};
 
 module.exports = {
   getUsuarios,
-  crearUsuarios
+  crearUsuarios,
 };
