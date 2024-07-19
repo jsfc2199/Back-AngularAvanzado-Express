094d8b905a0d75dae238a134df713fcf33fe7f70
Juanse Franco <jsfc2199@gmail.com>
2024-07-18 19:21:16 -0500

    creacion de primera ruta

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require('express')
 //crear servidor de express
 const app = express()
 
-
-
-
-
+//rutas
+app.get('/', (req, res) => {
+    res.status(200).json({
+        ok: true,
+        msg:'Hola'
+    })
+})
 
 app.listen(6321, ()=>{
     console.log('Servidor corriendo')
